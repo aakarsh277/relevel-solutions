ec4a12fe0807b763c6e9df53f3804c02234d0318
aakarsh277 <109575548+aakarsh277@users.noreply.github.com>
2022-09-13 10:54:12 +0530

    array.js

---
+++ array.js
@@ -0,0 +1,26 @@
+/*let marks = [22, 44, 98, 77, 49, 99, 87, 88, 19]
+const fruits = ['apple', 'orange', 'grapes', 'mango', 'strwaberry']
+marks[5] = 101
+console.log(marks);
+const mixed = ['sitafal', 88, [2, 3, 44, 'lullipulao']];
+console.log(marks.length);
+marks.push(778877);
+console.log(marks);
+marks.push(99999999999);
+console.log(marks);
+marks.push(1111111111111122222222222333333333333);
+console.log(marks);
+marks.unshift(112211);
+console.log(marks);
+marks.pop();
+console.log(marks);
+marks.pop();
+console.log(marks);*/
+let lulli = {
+    name: "sundar", car: "sedan", height: 6, marks: [22, 33, 44, 55, 65, 786]
+}
+
+console.log(lulli.marks);
+console.log(lulli.car);
+
+
